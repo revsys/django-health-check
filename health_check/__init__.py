d559bdcd10d8c3d3a22a2d8b918b3f30675a1895
Kristian Øllegaard <kristian.ollegaard@divio.ch>
2011-08-25 15:21:01 +0200

    Now a decent python package

--- health_check/__init__.py
+++ health_check/__init__.py
@@ -1,5 +1,14 @@
 # This file is heavily inspired from the django admin autodiscover
 
+__version_info__ = {
+    'major': 0,
+    'minor': 1,
+    'micro': 0,
+    'releaselevel': 'alpha',
+    'serial': 1
+}
+
+
 from health_check.plugins import plugin_dir
 
 def autodiscover():
@@ -32,3 +41,14 @@ def autodiscover():
             # attempting to import it, otherwise we want it to bubble up.
             if module_has_submodule(mod, 'plugin_health_check'):
                 raise
+
+def get_version(short=False):
+    assert __version_info__['releaselevel'] in ('alpha', 'beta', 'final')
+    vers = ["%(major)i.%(minor)i" % __version_info__, ]
+    if __version_info__['micro']:
+        vers.append(".%(micro)i" % __version_info__)
+    if __version_info__['releaselevel'] != 'final' and not short:
+        vers.append('%s%i' % (__version_info__['releaselevel'][0], __version_info__['serial']))
+    return ''.join(vers)
+
+__version__ = get_version()
