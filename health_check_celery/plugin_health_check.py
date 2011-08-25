443874d84fbe3072e625d11d2b823c8eac3b08a7
Kristian Øllegaard <kristian.ollegaard@divio.ch>
2011-08-25 14:25:05 +0200

    celery backend

---
+++ health_check_celery/plugin_health_check.py
@@ -0,0 +1,18 @@
+from health_check import plugin_dir
+from health_check.backends.base import BaseHealthCheckBackend, HealthCheckStatusType
+from health_check_celery.tasks import add
+from datetime import datetime, timedelta
+from time import sleep
+
+class CeleryHealthCheck(BaseHealthCheckBackend):
+
+    def check_status(self):
+        result = add.apply_async(args=[4,4], expires=datetime.now() + timedelta(seconds=3), connect_timeout=3)
+        now = datetime.now()
+        while (now + timedelta(seconds=3)) > datetime.now():
+            if result.result == 8:
+                return HealthCheckStatusType.working
+            sleep(0.5)
+        return HealthCheckStatusType.unavailable
+
+plugin_dir.register(CeleryHealthCheck)
---
+++ health_check_celery/tasks.py
@@ -0,0 +1,5 @@
+from celery.task import task
+
+@task
+def add(x, y):
+    return x + y
