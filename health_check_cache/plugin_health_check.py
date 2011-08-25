bdaa751c9d6c7d15f265be4166424d1e00922310
Kristian Øllegaard <kristian.ollegaard@divio.ch>
2011-08-25 14:24:58 +0200

    cache backend

---
+++ health_check_cache/plugin_health_check.py
@@ -0,0 +1,22 @@
+from django.core.cache.backends.base import CacheKeyWarning
+from health_check.backends.base import BaseHealthCheckBackend, HealthCheckStatusType
+from health_check import plugin_dir
+from django.core.cache import cache
+
+class CacheBackend(BaseHealthCheckBackend):
+
+    def check_status(self):
+        try:
+            cache.set('djangohealtcheck_test', 'itworks', 1)
+            if cache.get("djangohealtcheck_test") == "itworks":
+                return HealthCheckStatusType.working
+            else:
+                return HealthCheckStatusType.unavailable
+        except CacheKeyWarning:
+            return HealthCheckStatusType.unexpected_result
+        except ValueError:
+            return HealthCheckStatusType.unexpected_result
+        except:
+            return HealthCheckStatusType.unavailable
+
+plugin_dir.register(CacheBackend)
