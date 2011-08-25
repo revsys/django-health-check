720c74a790e4835198e8c210ff8fb2ba29ce28d6
Kristian Øllegaard <kristian.ollegaard@divio.ch>
2011-08-25 14:25:12 +0200

    django orm backend

---
+++ health_check_db/plugin_health_check.py
@@ -0,0 +1,20 @@
+from health_check.backends.base import BaseHealthCheckBackend, HealthCheckStatusType
+from health_check.models import TestModel
+from django.db import DatabaseError, IntegrityError
+from health_check import plugin_dir
+
+class DjangoDatabaseBackend(BaseHealthCheckBackend):
+
+    def check_status(self):
+        try:
+            obj = TestModel.objects.create(title="test")
+            obj.title = "newtest"
+            obj.save()
+            obj.delete()
+            return HealthCheckStatusType.working
+        except IntegrityError:
+            return HealthCheckStatusType.unexpected_result
+        except DatabaseError:
+            return HealthCheckStatusType.unavailable
+
+plugin_dir.register(DjangoDatabaseBackend)
