ae88cf327683ae8925ff3c7698e5916e6532f3e0
Kristian Øllegaard <kristian@kristian.io>
2013-03-18 18:39:46 +0100

    .identifier is a function and should be called.

--- health_check/views.py
+++ health_check/views.py
@@ -2,16 +2,17 @@ from django.http import HttpResponse, HttpResponseServerError
 from django.template import loader
 from health_check.plugins import plugin_dir
 from health_check.backends.base import HealthCheckStatusType
+
+
 def home(request):
     plugins = []
     working = True
-    
     for plugin_class, plugin in plugin_dir._registry.items():
         plugin = plugin_class()
         if plugin.status != HealthCheckStatusType.working:
             working = False
         plugins.append(plugin)
-    plugins.sort(key=lambda x: x.identifier)
+    plugins.sort(key=lambda x: x.identifier())
 
     if working:
         return HttpResponse(loader.render_to_string("health_check/dashboard.html", {'plugins': plugins}))
