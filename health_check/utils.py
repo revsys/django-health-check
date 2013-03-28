aa50b96efda758348287925a7045003b1377eb51
Jonas Obrist <ojiidotch@gmail.com>
2013-03-28 17:44:29 +0900

    updated docstrings, removed custom exceptions

--- health_check/utils.py
+++ health_check/utils.py
@@ -6,21 +6,8 @@ from health_check.plugins import plugin_dir
 
 class BaseHealthCheck(BaseHealthCheckBackend):
     def check_status(self):
-        try:
-            self._wrapped()
-        except HealthCheckFailed:
-            return HealthCheckStatusType.unexpected_result
-        except HealthCheckUnavailable:
-            return HealthCheckStatusType.unavailable
-        except:
-            return HealthCheckStatusType.unexpected_result
+        self._wrapped()
         return HealthCheckStatusType.working
-
-class HealthCheckFailed(Exception):
-    pass
-
-class HealthCheckUnavailable(Exception):
-    pass
 
 
 def healtcheck(func_or_name):
@@ -30,12 +17,12 @@ def healtcheck(func_or_name):
         @healthcheck("My Check")
         def my_check():
             if something_is_not_okay():
-                raise HealthCheckFailed()
+                raise ServiceReturnedUnexpectedResult()
 
         @healthcheck
         def other_check():
             if something_is_not_available():
-                raise HealthCHeckUnavailable()
+                raise ServiceUnavailable()
     """
     def inner(func):
         cls = type(func.__name__, (BaseHealthCheck,), {'_wrapped': func})
