8253c8ca6df94a8d818a01162724474a8ab47e92
phil.roche <phil.roche@ticket-text.com>
2014-02-07 16:43:28 +0000

    Added new view to return plugin status as JSON

--- health_check/views.py
+++ health_check/views.py
@@ -1,3 +1,6 @@
+
+import json
+
 from django.http import HttpResponse, HttpResponseServerError
 from django.template import loader
 from health_check.plugins import plugin_dir
@@ -17,3 +20,20 @@ def home(request):
         return HttpResponse(loader.render_to_string("health_check/dashboard.html", {'plugins': plugins}))
     else:
         return HttpResponseServerError(loader.render_to_string("health_check/dashboard.html", {'plugins': plugins}))
+
+def json(request):
+    plugins = []
+    working = True
+    for plugin_class, plugin in plugin_dir._registry.items():
+        plugin = plugin_class()
+        if not plugin.status:  # Will return True or None
+            working = False
+        plugins.append(plugin)
+    plugins.sort(key=lambda x: x.identifier())
+
+    health_check_status = {}
+    for plugin in plugins:
+            health_check_status[str(plugin.identifier())] = str(plugin.pretty_status())
+
+    health_check_json = json.dumps(health_check_status)
+    return HttpResponse(health_check_json, mimetype='application/json')
