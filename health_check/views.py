31a02f1b39d547bb577632be4f270265ed9573dd
phil.roche <phil.roche@ticket-text.com>
2014-02-07 16:58:41 +0000

    created new method get_plugins in utils to remove code duplication in views.py

--- health_check/views.py
+++ health_check/views.py
@@ -3,18 +3,11 @@ import json
 
 from django.http import HttpResponse, HttpResponseServerError
 from django.template import loader
-from health_check.plugins import plugin_dir
+from health_check.utils import get_plugins
 
 
 def home(request):
-    plugins = []
-    working = True
-    for plugin_class, plugin in plugin_dir._registry.items():
-        plugin = plugin_class()
-        if not plugin.status:  # Will return True or None
-            working = False
-        plugins.append(plugin)
-    plugins.sort(key=lambda x: x.identifier())
+    plugins, working = get_plugins()
 
     if working:
         return HttpResponse(loader.render_to_string("health_check/dashboard.html", {'plugins': plugins}))
@@ -22,14 +15,7 @@ def home(request):
         return HttpResponseServerError(loader.render_to_string("health_check/dashboard.html", {'plugins': plugins}))
 
 def jsonhealthcheck(request):
-    plugins = []
-    working = True
-    for plugin_class, plugin in plugin_dir._registry.items():
-        plugin = plugin_class()
-        if not plugin.status:  # Will return True or None
-            working = False
-        plugins.append(plugin)
-    plugins.sort(key=lambda x: x.identifier())
+    plugins, working = get_plugins()
 
     health_check_status = {}
     for plugin in plugins:
