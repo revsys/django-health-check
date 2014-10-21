a2f12921ae44db364a4de3ac595e54ddb6c627fa
Stefan Foulis <stefan@foulis.ch>
2014-10-21 10:32:49 +0200

    adds health_check_celery3 to package

--- setup.py
+++ setup.py
@@ -23,7 +23,15 @@ setup(
     license="BSD",
     keywords="django health check monitoring",
     url="https://github.com/KristianOellegaard/django-health-check",
-    packages=['health_check', 'health_check_celery', 'health_check_db', 'health_check_cache', 'health_check.backends', 'health_check_storage'],
+    packages=[
+        'health_check',
+        'health_check_celery',
+        'health_check_celery3',
+        'health_check_db',
+        'health_check_cache',
+        'health_check.backends',
+        'health_check_storage',
+    ],
     long_description=read('README.md'),
     classifiers=[
         "Topic :: Utilities",
