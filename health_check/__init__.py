608e0cae6b33ed26bcffa5e16ba49305119a7b20
Sami Fahed <samifah@gmail.com>
2015-12-11 19:21:46 +0100

    Added django1.9 compatibility

--- health_check/__init__.py
+++ health_check/__init__.py
@@ -18,7 +18,7 @@ def autodiscover():
 
     import copy
     from django.conf import settings
-    from django.utils.importlib import import_module
+    from importlib import import_module
     from django.utils.module_loading import module_has_submodule
     from health_check.plugins import plugin_dir
 
