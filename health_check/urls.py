588d3e7dc20d06c07dd196999bd3546d92f4f816
David Burke <dmbst32@gmail.com>
2016-08-16 15:58:55 -0400

    Django 1.10 compatible urls.py
    Fixes #46

--- health_check/urls.py
+++ health_check/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
+from health_check.views import home
 
 import health_check
 
 health_check.autodiscover()
 
 urlpatterns = [
-    url(r'^$', 'health_check.views.home', name='health_check_home'),
+    url(r'^$', home, name='health_check_home'),
 ]
