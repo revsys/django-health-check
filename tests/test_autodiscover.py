64811fb374fe8a1d8994c88dcbd1ce63716b8ad6
Mark Speedy <mspeedy@choicestream.com>
2016-08-29 11:58:17 -0400

    Fixed styling so tox -e qa succeeds

--- tests/test_autodiscover.py
+++ tests/test_autodiscover.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
+
 from health_check.plugins import plugin_dir
 
 
 class TestAutoDiscover(object):
     def test_autodiscover(self):
-        assert len(plugin_dir._registry) == 0
+        health_check_plugins = list(filter(
+            lambda x: 'health_check_' in x,
+            settings.INSTALLED_APPS
+        ))
+
+        assert len(plugin_dir._registry) == len(health_check_plugins)
