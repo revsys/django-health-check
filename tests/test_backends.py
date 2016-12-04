5d7b2ef545b4f115b80317c952ea56c362d489da
Aleksi Häkli <aleksi.hakli@vincit.fi>
2016-12-04 15:50:25 +0200

    Add basic tests for the new error and logger methods

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
-from health_check.backends.base import BaseHealthCheckBackend
+import pytest
+
+from health_check.backends.base import (
+    BaseHealthCheckBackend, ServiceReturnedUnexpectedResult, ServiceUnavailable
+)
 
 
 class TestBaseHealthCheckBackend(object):
-
     def test_status(self):
         assert BaseHealthCheckBackend().status is None
+
+    def test_error(self):
+        with pytest.raises(Exception):
+            BaseHealthCheckBackend().error(Exception, '.error() raises correctly', None)
+
+    def test_unavailable(self):
+        with pytest.raises(ServiceUnavailable):
+            BaseHealthCheckBackend().unavailable('.unavailable() raises correctly')
+
+    def test_unexpected(self):
+        with pytest.raises(ServiceReturnedUnexpectedResult):
+            BaseHealthCheckBackend().unexpected('.unexpected() raises correctly')
