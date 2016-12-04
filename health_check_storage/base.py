b3080fcd9fd47ad9aa076bd791c94374aa535328
Aleksi Häkli <aleksi.hakli@vincit.fi>
2016-12-04 15:20:48 +0200

    Refactor StorageHealthCheck backend
    Split save and delete functionality into check_save
    and check_delete methods to allow for easier subclassing.
    
    This does not break backwards compatibility.
    If someone has previously overridden check_status,
    check_save and check_delete will simply not be used.

--- health_check_storage/base.py
+++ health_check_storage/base.py
@@ -42,27 +42,33 @@ class StorageHealthCheck(BaseHealthCheckBackend):
     def get_file_content(self):
         return b'this is the healthtest file content'
 
+    def check_save(self, file_name, file_content):
+        storage = self.get_storage()
+        # save the file
+        file_name = storage.save(
+            file_name, ContentFile(content=file_content)
+        )
+        # read the file and compare
+        if not storage.exists(file_name):
+            self.unavailable('File does not exist')
+        with storage.open(file_name) as f:
+            if not f.read() == file_content:
+                self.unavailable('File content does not match')
+
+    def check_delete(self, file_name):
+        storage = self.get_storage()
+        # delete the file and make sure it is gone
+        storage.delete(file_name)
+        if storage.exists(file_name):
+            self.unavailable('File was not deleted')
+
     def check_status(self):
         try:
             # write the file to the storage backend
-            storage = self.get_storage()
             file_name = self.get_file_name()
             file_content = self.get_file_content()
-
-            # save the file
-            file_name = storage.save(
-                file_name, ContentFile(content=file_content)
-            )
-            # read the file and compare
-            if not storage.exists(file_name):
-                self.unavailable('File does not exist')
-            with storage.open(file_name) as f:
-                if not f.read() == file_content:
-                    self.unavailable('File content does not match')
-            # delete the file and make sure it is gone
-            storage.delete(file_name)
-            if storage.exists(file_name):
-                self.unavailable('File was not deleted')
+            self.check_save(file_name, file_content)
+            self.check_delete(file_name)
             return True
         except Exception as e:
             self.unavailable('Unknown exception {}'.format(e))
