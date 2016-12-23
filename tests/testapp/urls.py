6d7e676e9951dccf01f611c2917d0ea9f7ca630f
Aleksi Häkli <aleksi.hakli@gmail.com>
2016-12-23 13:09:23 +0200

    Add tests for health check views

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-urlpatterns = []
+from django.conf.urls import include, url
+
+urlpatterns = [
+    url('^ht/', include('health_check.urls')),
+]
