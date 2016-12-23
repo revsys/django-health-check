c212a3f5dc6d8b4157206ecbf5a01b5091261671
J C Lawrence <jcl@matterport.com>
2016-12-23 11:47:46 +0200

    Added support for a JSON health endpoint

--- health_check/urls.py
+++ health_check/urls.py
@@ -2,10 +2,11 @@
 from django.conf.urls import url
 
 import health_check
-from health_check.views import home
+from health_check.views import home, json_view
 
 health_check.autodiscover()
 
 urlpatterns = [
     url(r'^$', home, name='health_check_home'),
+    url(r'^json/?$', json_view, name='health_check_json_view')
 ]
