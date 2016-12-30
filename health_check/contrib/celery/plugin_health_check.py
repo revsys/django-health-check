6bf88a17540ce94204a03362d84d171e7aad7074
Johannes Hoppe <info@johanneshoppe.com>
2016-12-30 14:00:47 +0100

    Resolve #74 -- Use result.get instastad of ready
    Use celery API for result polling over own implementaiton.

--- health_check/contrib/celery/plugin_health_check.py
+++ health_check/contrib/celery/plugin_health_check.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime, timedelta
-from time import sleep
 
 from django.conf import settings
 
 from health_check.backends.base import (
-    BaseHealthCheckBackend, ServiceUnavailable
-)
+    BaseHealthCheckBackend, ServiceUnavailable,
+    ServiceReturnedUnexpectedResult)
 from health_check.plugins import plugin_dir
 from .tasks import add
 
 
 class CeleryHealthCheck(BaseHealthCheckBackend):
-
     def check_status(self):
         timeout = getattr(settings, 'HEALTHCHECK_CELERY_TIMEOUT', 3)
 
@@ -21,22 +19,13 @@ class CeleryHealthCheck(BaseHealthCheckBackend):
                 args=[4, 4],
                 expires=datetime.now() + timedelta(seconds=timeout)
             )
-            now = datetime.now()
-            while (now + timedelta(seconds=3)) > datetime.now():
-                print("            checking....")
-                if result.ready():
-                    try:
-                        result.forget()
-                    except NotImplementedError:
-                        pass
-                    return True
-                sleep(0.5)
-        except IOError:
-            raise ServiceUnavailable("IOError")
-        except:
-            raise ServiceUnavailable("Unknown error")
-
-        raise ServiceUnavailable("Unknown error")
+            result.get(timeout=timeout)
+            if result.result != 8:
+                self.add_error(ServiceReturnedUnexpectedResult("Celery return wrong result"))
+        except IOError as e:
+            self.add_error(ServiceUnavailable("IOError"), e)
+        except BaseException as e:
+            self.add_error(ServiceUnavailable("Unknown error"), e)
 
 
 plugin_dir.register(CeleryHealthCheck)
