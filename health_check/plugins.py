4a8973078ac7e43dacd16caac1b815c0162686de
Johannes Hoppe <info@johanneshoppe.com>
2017-01-07 14:29:38 +0100

    Allow registering the same plugin multiple times

--- health_check/plugins.py
+++ health_check/plugins.py
@@ -21,16 +21,8 @@ class HealthCheckPluginDirectory(object):
 
     def register(self, plugin, **options):
         """Add the given plugin from the registry."""
-        if plugin in self._registry:
-            raise AlreadyRegistered('The plugin %s is already registered' % plugin.__name__)
         # Instantiate the admin class to save in the registry
         self._registry[plugin] = options
-
-    def unregister(self, plugin):
-        """Remove the given plugin from the registry."""
-        if plugin not in self._registry:
-            raise NotRegistered('The plugin %s is not registered' % plugin.__name__)
-        del self._registry[plugin]
 
 
 plugin_dir = HealthCheckPluginDirectory()
