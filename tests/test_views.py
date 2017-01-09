c26133bf0a10f462edccea531843765f6e0da21f
Aleksi Häkli <aleksi.hakli@gmail.com>
2017-01-09 15:16:44 +0200

    JSON healthcheck tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import json
 
 from health_check.backends import BaseHealthCheckBackend
 from health_check.plugins import plugin_dir
@@ -22,7 +23,31 @@ class TestMainView(object):
             def run_check(self):
                 self.add_error('Super Fail!')
 
+        plugin_dir.reset()
         plugin_dir.register(MyBackend)
         response = client.get(self.url)
         assert response.status_code == 500, response.content.decode('utf-8')
         assert b'Super Fail!' in response.content
+
+    def test_success_json(self, client):
+        class JSONSuccessBackend(BaseHealthCheckBackend):
+            def run_check(self):
+                pass
+
+        plugin_dir.reset()
+        plugin_dir.register(JSONSuccessBackend)
+        response = client.get(self.url, HTTP_ACCEPT='application/json')
+        assert response.status_code == 200, response.content.decode('utf-8')
+        assert json.loads(response.content.decode('utf-8')) == \
+            {JSONSuccessBackend().identifier(): JSONSuccessBackend().pretty_status()}
+
+    def test_error_json(self, client):
+        class JSONErrorBackend(BaseHealthCheckBackend):
+            def run_check(self):
+                self.add_error('JSON Error')
+
+        plugin_dir.reset()
+        plugin_dir.register(JSONErrorBackend)
+        response = client.get(self.url, HTTP_ACCEPT='application/json')
+        assert response.status_code == 500, response.content.decode('utf-8')
+        assert 'JSON Error' in json.loads(response.content.decode('utf-8'))[JSONErrorBackend().identifier()]
