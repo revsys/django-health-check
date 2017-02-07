808b252ea4d99f2e0b2a313bf8b3d097e4cc6ab7
Craig Weber <crgwbr@gmail.com>
2017-02-07 15:21:47 -0500

    Send timezone-aware datetime for task expiry
    Needed because this task would consistantly fail if django is set to a
    later-than-UTC timezone, due to celery thinking the task expired the instant
    it's sent.

--- health_check/contrib/celery/backends.py
+++ health_check/contrib/celery/backends.py
@@ -1,6 +1,7 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 from django.conf import settings
+from django.utils import timezone
 
 from health_check.backends import BaseHealthCheckBackend
 from health_check.exceptions import (
@@ -17,7 +18,7 @@ class CeleryHealthCheck(BaseHealthCheckBackend):
         try:
             result = add.apply_async(
                 args=[4, 4],
-                expires=datetime.now() + timedelta(seconds=timeout)
+                expires=timezone.now() + timedelta(seconds=timeout)
             )
             result.get(timeout=timeout)
             if result.result != 8:
