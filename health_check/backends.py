ea71142b9156c670532fddc6adadbacfee8d8f6f
carderm <michael.carder@michaelcarder.co.uk>
2017-08-21 16:09:41 +0100

    Added time_taken to backend and index.html template.

--- health_check/backends.py
+++ health_check/backends.py
@@ -9,6 +9,8 @@ from django.utils.translation import ugettext_lazy as _
 
 from health_check.exceptions import HealthCheckException
 
+from timeit import default_timer as timer
+
 logger = logging.getLogger('health-check')
 
 
