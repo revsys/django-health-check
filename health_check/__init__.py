303a9ccd6951f02ff85f125affe6a8d585721b72
Johannes Hoppe <info@johanneshoppe.com>
2017-12-13 11:14:37 +0100

    Switch to pbr for package setup

--- health_check/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Used by setup.py, so minimize top-level imports.
-
-VERSION = (2, 4, 0)
-
-__version__ = ".".join(str(i) for i in VERSION)
--- setup.py
+++ setup.py
@@ -1,50 +1,8 @@
-# -*- coding: utf-8 -*-
-import os
+#!/usr/bin/env python
 
-from setuptools import find_packages, setup
-
-from health_check import __version__
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
+from setuptools import setup
 
 setup(
-    name="django-health-check",
-    version=__version__,
-    author="Kristian Ollegaard",
-    author_email="kristian@oellegaard.com",
-    description=("a pluggable app that runs a full check on the deployment,"
-                 " using a number of plugins to check e.g. database, queue server, celery processes, etc."),
-    license="BSD",
-    keywords="django health check monitoring",
-    url="https://github.com/KristianOellegaard/django-health-check",
-    packages=find_packages(exclude=['tests', 'docs']),
-    long_description=read('README.rst'),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
-        'Framework :: Django :: 1.10',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'License :: Public Domain',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Quality Assurance',
-        'Topic :: System :: Logging',
-        'Topic :: System :: Monitoring',
-        'Topic :: Utilities',
-    ],
-    install_requires=[],
-    include_package_data=True,
-    zip_safe=False,
+    setup_requires=['pbr'],
+    pbr=True,
 )
