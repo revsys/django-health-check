c4481ee02b77e49ebe5494fe1bd93c8de0756144
Witold Greń <witold.gren@gmail.com>
2019-01-07 00:10:57 +0100

    fixed flake8 errors, fixed autodiscover test and reset discover celery queues test

--- tests/test_autodiscover.py
+++ tests/test_autodiscover.py
@@ -7,17 +7,19 @@ from health_check.plugins import plugin_dir
 
 
 class TestAutoDiscover:
+
     def test_autodiscover(self):
         health_check_plugins = list(filter(
-            lambda x: 'health_check.' in x and 'celery' or 'celery_ping' not in x,
+            lambda x: x.startswith('health_check.') and 'celery' not in x,
             settings.INSTALLED_APPS
         ))
 
-        non_celery_plugins = [x for x in plugin_dir._registry if not issubclass(x[0], (CeleryHealthCheck, CeleryPingHealthCheck))]
+        non_celery_plugins = [x for x in plugin_dir._registry
+                              if not issubclass(x[0], (CeleryHealthCheck, CeleryPingHealthCheck))]
 
         # The number of installed apps excluding celery should equal to all plugins except celery
         assert len(non_celery_plugins) == len(health_check_plugins)
 
     def test_discover_celery_queues(self):
-        celery_plugins = [x for x in plugin_dir._registry if issubclass(x[0], (CeleryHealthCheck, CeleryPingHealthCheck))]
-        assert len(celery_plugins) == 2
+        celery_plugins = [x for x in plugin_dir._registry if issubclass(x[0], CeleryHealthCheck)]
+        assert len(celery_plugins) == len(current_app.amqp.queues)
