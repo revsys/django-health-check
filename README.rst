b0d65ed778e69f8667756a67ac79acda4c5273ce
Witold Greń <witold.gren@gmail.com>
2019-01-06 22:39:43 +0100

    fix isort, replaced mocker to mock, updated docs

--- README.rst
+++ README.rst
@@ -70,6 +70,7 @@ Add the ``health_check`` applications to your ``INSTALLED_APPS``:
         'health_check.storage',
         'health_check.contrib.migrations',
         'health_check.contrib.celery',              # requires celery
+        'health_check.contrib.celery_ping',         # requires celery
         'health_check.contrib.psutil',              # disk and memory utilization; requires psutil
         'health_check.contrib.s3boto3_storage',     # requires boto3 and S3BotoStorage backend
         'health_check.contrib.rabbitmq',            # requires RabbitMQ broker
--- tests/test_celery_ping.py
+++ tests/test_celery_ping.py
@@ -1,6 +1,7 @@
 import pytest
 
 from health_check.contrib.celery_ping.backends import CeleryPingHealthCheck
+from mock import patch
 
 
 class TestCeleryWorkersHealthCheck:
@@ -8,32 +9,27 @@ class TestCeleryWorkersHealthCheck:
     CELERY_HEALTH_CHECK_MODULE = \
         'health_check.contrib.celery_ping.backends.app.control.ping'
 
-    def test_check_status_doesnt_add_errors_when_ping_successfull(
-            self, mocker):
-        mocker.patch(
-            self.CELERY_HEALTH_CHECK_MODULE,
-            return_value=[{'celery@4cc150a7b49b': {'ok': 'pong'}}])
+    def test_check_status_doesnt_add_errors_when_ping_successfull(self):
+        with patch(self.CELERY_HEALTH_CHECK_MODULE,
+                   return_value=[{'celery@4cc150a7b49b': {'ok': 'pong'}}]):
+            health_check = self.health_check_class()
+            health_check.check_status()
 
-        health_check = self.health_check_class()
-        health_check.check_status()
-
-        assert len(health_check.errors) == 0
+            assert len(health_check.errors) == 0
 
     @pytest.mark.parametrize('exception_to_raise', [
         IOError,
         TimeoutError,
     ])
     def test_check_status_add_error_when_io_error_raised_from_ping(
-            self, mocker, exception_to_raise):
-        mocker.patch(
-            self.CELERY_HEALTH_CHECK_MODULE,
-            side_effect=exception_to_raise)
+            self, exception_to_raise):
+        with patch(self.CELERY_HEALTH_CHECK_MODULE,
+                   side_effect=exception_to_raise):
+            health_check = self.health_check_class()
+            health_check.check_status()
 
-        health_check = self.health_check_class()
-        health_check.check_status()
-
-        assert len(health_check.errors) == 1
-        assert 'ioerror' in health_check.errors[0].message.lower()
+            assert len(health_check.errors) == 1
+            assert 'ioerror' in health_check.errors[0].message.lower()
 
     @pytest.mark.parametrize('exception_to_raise', [
         ValueError,
@@ -42,29 +38,25 @@ class TestCeleryWorkersHealthCheck:
         MemoryError,
     ])
     def test_check_status_add_error_when_any_exception_raised_from_ping(
-            self, mocker, exception_to_raise):
-        mocker.patch(
-            self.CELERY_HEALTH_CHECK_MODULE,
-            side_effect=exception_to_raise)
+            self, exception_to_raise):
+        with patch(self.CELERY_HEALTH_CHECK_MODULE,
+                   side_effect=exception_to_raise):
+            health_check = self.health_check_class()
+            health_check.check_status()
 
-        health_check = self.health_check_class()
-        health_check.check_status()
-
-        assert len(health_check.errors) == 1
-        assert health_check.errors[0].message.lower() == 'unknown error'
+            assert len(health_check.errors) == 1
+            assert health_check.errors[0].message.lower() == 'unknown error'
 
     @pytest.mark.parametrize('ping_result', [
         None,
         list()
     ])
     def test_check_status_add_error_when_ping_result_failed(
-            self, mocker, ping_result):
-        mocker.patch(
-            self.CELERY_HEALTH_CHECK_MODULE,
-            return_value=ping_result)
+            self, ping_result):
+        with patch(self.CELERY_HEALTH_CHECK_MODULE,
+                   return_value=ping_result):
+            health_check = self.health_check_class()
+            health_check.check_status()
 
-        health_check = self.health_check_class()
-        health_check.check_status()
-
-        assert len(health_check.errors) == 1
-        assert 'workers unavailable' in health_check.errors[0].message.lower()
+            assert len(health_check.errors) == 1
+            assert 'workers unavailable' in health_check.errors[0].message.lower()
