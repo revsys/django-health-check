02cd1365fe956458214864afdae3e053ff495351
Witold Greń <witold.gren@gmail.com>
2019-01-06 21:48:05 +0100

    created new health check for celery (celery ping)

---
+++ health_check/contrib/celery_ping/backends.py
@@ -0,0 +1,33 @@
+from django.conf import settings
+
+try:
+    from celery.app import default_app as app
+except ImportError:
+    from celery import current_app as app
+
+from health_check.backends import BaseHealthCheckBackend
+from health_check.exceptions import ServiceUnavailable
+
+
+class CeleryPingHealthCheck(BaseHealthCheckBackend):
+
+    def check_status(self):
+        timeout = getattr(settings, 'HEALTHCHECK_CELERY_PING_TIMEOUT', 1)
+        try:
+            ping_result = app.control.ping(timeout=timeout)
+        except IOError as e:
+            self.add_error(ServiceUnavailable('IOError'), e)
+        except NotImplementedError as exc:
+            self.add_error(
+                ServiceUnavailable(
+                    'NotImplementedError: Make sure CELERY_RESULT_BACKEND is set'
+                ),
+                exc,
+            )
+        except BaseException as exc:
+            self.add_error(ServiceUnavailable('Unknown error'), exc)
+        else:
+            if not ping_result:
+                self.add_error(
+                    ServiceUnavailable('Celery workers unavailable'),
+                )
---
+++ tests/test_celery_ping.py
@@ -0,0 +1,72 @@
+import pytest
+
+from health_checks.contrib.celery_ping.backends import CeleryPingHealthCheck
+
+CELERY_HEALTH_CHECK_MODULE = 'apps.health_checks.backends.celery.'
+
+
+class TestCeleryWorkersHealthCheck:
+    health_check_class = CeleryPingHealthCheck
+    CELERY_HEALTH_CHECK_MODULE = \
+        'health_checks.contrib.celery_ping.backends.app.control.ping'
+
+    def test_check_status_doesnt_add_errors_when_ping_successfull(
+            self, mocker):
+        mocker.patch(
+            self.CELERY_HEALTH_CHECK_MODULE,
+            return_value=[{'celery@4cc150a7b49b': {'ok': 'pong'}}])
+
+        health_check = self.health_check_class()
+        health_check.check_status()
+
+        assert len(health_check.errors) == 0
+
+    @pytest.mark.parametrize('exception_to_raise', [
+        IOError,
+        TimeoutError,
+    ])
+    def test_check_status_add_error_when_io_error_raised_from_ping(
+            self, mocker, exception_to_raise):
+        mocker.patch(
+            self.CELERY_HEALTH_CHECK_MODULE,
+            side_effect=exception_to_raise)
+
+        health_check = self.health_check_class()
+        health_check.check_status()
+
+        assert len(health_check.errors) == 1
+        assert 'ioerror' in health_check.errors[0].message.lower()
+
+    @pytest.mark.parametrize('exception_to_raise', [
+        ValueError,
+        SystemError,
+        IndexError,
+        MemoryError,
+    ])
+    def test_check_status_add_error_when_any_exception_raised_from_ping(
+            self, mocker, exception_to_raise):
+        mocker.patch(
+            self.CELERY_HEALTH_CHECK_MODULE,
+            side_effect=exception_to_raise)
+
+        health_check = self.health_check_class()
+        health_check.check_status()
+
+        assert len(health_check.errors) == 1
+        assert health_check.errors[0].message.lower() == 'unknown error'
+
+    @pytest.mark.parametrize('ping_result', [
+        None,
+        list()
+    ])
+    def test_check_status_add_error_when_ping_result_failed(
+            self, mocker, ping_result):
+        mocker.patch(
+            self.CELERY_HEALTH_CHECK_MODULE,
+            return_value=ping_result)
+
+        health_check = self.health_check_class()
+        health_check.check_status()
+
+        assert len(health_check.errors) == 1
+        assert 'workers unavailable' in health_check.errors[0].message.lower()
