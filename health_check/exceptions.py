be6cc247c12c3ac682d796bcf79a9ead530ec36c
Jamie Hewland <jhewland@gmail.com>
2019-04-23 13:27:05 +0200

    Remove usage of deprecated ugettext_lazy

--- health_check/exceptions.py
+++ health_check/exceptions.py
@@ -1,4 +1,4 @@
-from django.utils.translation import ugettext_lazy as _  # noqa: N812
+from django.utils.translation import gettext_lazy as _  # noqa: N812
 
 
 class HealthCheckException(Exception):
