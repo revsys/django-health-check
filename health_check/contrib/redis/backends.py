2f6223b8e419b8551576d3047c0c2e611517c7ab
Ingvald Lorentzen <ingvald.lorentzen@intility.no>
2019-06-27 08:42:11 +0200

    - Added Redis health check, inspired by the RabbitMQ healthcheck - Fixed typo in RedisMQ logger message

--- health_check/contrib/redis/backends.py
+++ health_check/contrib/redis/backends.py
@@ -1,6 +1,5 @@
 import logging
 
-from amqp.exceptions import AccessRefused
 from django.conf import settings
 from kombu import Connection
 
@@ -10,27 +9,27 @@ from health_check.exceptions import ServiceUnavailable
 logger = logging.getLogger(__name__)
 
 
-class RabbitMQHealthCheck(BaseHealthCheckBackend):
-    """Health check for RabbitMQ."""
+class RedisHealthCheck(BaseHealthCheckBackend):
+    """Health check for Redis."""
 
     def check_status(self):
-        """Check RabbitMQ service by opening and closing a broker channel."""
+        """Check Redis service by opening and closing a broker channel."""
         logger.debug("Checking for a broker_url on django settings...")
 
-        broker_url = getattr(settings, "BROKER_URL", None)
+        broker_url = getattr(settings, "CELERY_BROKER_URL", None)
 
-        logger.debug("Got %s as the broker_url. Connecting to rabbit...", broker_url)
+        logger.debug("Got %s as the broker_url. Connecting to redis...", broker_url)
 
-        logger.debug("Attempting to connect to rabbit...")
+        logger.debug("Attempting to connect to redis...")
         try:
             # conn is used as a context to release opened resources later
             with Connection(broker_url) as conn:
                 conn.connect()  # exceptions may be raised upon calling connect
         except ConnectionRefusedError as e:
-            self.add_error(ServiceUnavailable("Unable to connect to RabbitMQ: Connection was refused."), e)
+            self.add_error(ServiceUnavailable("Unable to connect to Redis: Connection was refused."), e)
 
-        except AccessRefused as e:
-            self.add_error(ServiceUnavailable("Unable to connect to RabbitMQ: Authentication error."), e)
+        # except AccessRefused as e:
+        #     self.add_error(ServiceUnavailable("Unable to connect to RabbitMQ: Authentication error."), e)
 
         except IOError as e:
             self.add_error(ServiceUnavailable("IOError"), e)
@@ -38,4 +37,4 @@ class RabbitMQHealthCheck(BaseHealthCheckBackend):
         except BaseException as e:
             self.add_error(ServiceUnavailable("Unknown error"), e)
         else:
-            logger.debug("Connection established. RabbitMQ is healthy.")
+            logger.debug("Connection established. Redis is healthy.")
