de75f9824c9ec6907e686c50c0637175044a0c49
ingvaldlorentzen <ingvald_lorentzen+github@me.com>
2019-06-29 13:05:06 +0200

    - Removed commented code - Generalized Django settings Redis URL name - Updated variable naming for redis health check

--- health_check/contrib/redis/backends.py
+++ health_check/contrib/redis/backends.py
@@ -13,23 +13,20 @@ class RedisHealthCheck(BaseHealthCheckBackend):
     """Health check for Redis."""
 
     def check_status(self):
-        """Check Redis service by opening and closing a broker channel."""
-        logger.debug("Checking for a broker_url on django settings...")
+        """Check Redis service by opening and closing a connection."""
+        logger.debug("Checking for a redis_url in django settings...")
 
-        broker_url = getattr(settings, "CELERY_BROKER_URL", None)
+        redis_url = getattr(settings, "REDIS_URL", None)
 
-        logger.debug("Got %s as the broker_url. Connecting to redis...", broker_url)
+        logger.debug("Got %s as the redis_url. Connecting to redis...", redis_url)
 
         logger.debug("Attempting to connect to redis...")
         try:
             # conn is used as a context to release opened resources later
-            with Connection(broker_url) as conn:
+            with Connection(redis_url) as conn:
                 conn.connect()  # exceptions may be raised upon calling connect
         except ConnectionRefusedError as e:
             self.add_error(ServiceUnavailable("Unable to connect to Redis: Connection was refused."), e)
-
-        # except AccessRefused as e:
-        #     self.add_error(ServiceUnavailable("Unable to connect to RabbitMQ: Authentication error."), e)
 
         except IOError as e:
             self.add_error(ServiceUnavailable("IOError"), e)
