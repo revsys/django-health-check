4310c58941695e1e20db343447925ded67f8f0a0
Jonas Krüger Svensson <jonas-ks@hotmail.com>
2019-08-23 15:22:57 +0200

    Added tests for new redis usage. Tox QA is behaving strangely, going to let Travis run it once.

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,5 +1,5 @@
 import mock
-from kombu.exceptions import ConnectionLimitExceeded, TimeoutError
+from redis.exceptions import ConnectionError, TimeoutError
 
 from health_check.contrib.redis.backends import RedisHealthCheck
 
@@ -8,15 +8,14 @@ class TestRedisHealthCheck:
     """Test Redis health check."""
 
     @mock.patch("health_check.contrib.redis.backends.getattr")
-    @mock.patch("health_check.contrib.redis.backends.Connection")
+    @mock.patch("health_check.contrib.redis.backends.from_url", autospec=True)
     def test_redis_refused_connection(self, mocked_connection, mocked_getattr):
         """Test when the connection to Redis is refused."""
         mocked_getattr.return_value = "redis_url"
 
         # mock returns
-        mocked_conn = mock.MagicMock()
-        mocked_connection.return_value.__enter__.return_value = mocked_conn
-        mocked_conn.connect.side_effect = ConnectionRefusedError("Refused connection")
+        mocked_connection.return_value = mock.MagicMock()
+        mocked_connection.return_value.__enter__.side_effect = ConnectionRefusedError("Refused connection")
 
         # instantiates the class
         redis_healthchecker = RedisHealthCheck()
@@ -29,15 +28,14 @@ class TestRedisHealthCheck:
         mocked_connection.assert_called_once_with("redis_url")
 
     @mock.patch("health_check.contrib.redis.backends.getattr")
-    @mock.patch("health_check.contrib.redis.backends.Connection")
+    @mock.patch("health_check.contrib.redis.backends.from_url")
     def test_redis_timeout_error(self, mocked_connection, mocked_getattr):
         """Test Redis TimeoutError."""
         mocked_getattr.return_value = "redis_url"
 
         # mock returns
-        mocked_conn = mock.MagicMock()
-        mocked_connection.return_value.__enter__.return_value = mocked_conn
-        mocked_conn.connect.side_effect = TimeoutError("Timeout Error")
+        mocked_connection.return_value = mock.MagicMock()
+        mocked_connection.return_value.__enter__.side_effect = TimeoutError("Timeout Error")
 
         # instantiates the class
         redis_healthchecker = RedisHealthCheck()
@@ -50,15 +48,14 @@ class TestRedisHealthCheck:
         mocked_connection.assert_called_once_with("redis_url")
 
     @mock.patch("health_check.contrib.redis.backends.getattr")
-    @mock.patch("health_check.contrib.redis.backends.Connection")
+    @mock.patch("health_check.contrib.redis.backends.from_url")
     def test_redis_con_limit_exceeded(self, mocked_connection, mocked_getattr):
         """Test Connection Limit Exceeded error."""
         mocked_getattr.return_value = "redis_url"
 
         # mock returns
-        mocked_conn = mock.MagicMock()
-        mocked_connection.return_value.__enter__.return_value = mocked_conn
-        mocked_conn.connect.side_effect = ConnectionLimitExceeded("Connection limit exceeded")
+        mocked_connection.return_value = mock.MagicMock()
+        mocked_connection.return_value.__enter__.side_effect = ConnectionError("Connection Error")
 
         # instantiates the class
         redis_healthchecker = RedisHealthCheck()
@@ -69,3 +66,24 @@ class TestRedisHealthCheck:
 
         # mock assertions
         mocked_connection.assert_called_once_with("redis_url")
+
+    @mock.patch("health_check.contrib.redis.backends.getattr")
+    @mock.patch("health_check.contrib.redis.backends.from_url")
+    def test_redis_conn_ok(self, mocked_connection, mocked_getattr):
+        """Test everything is OK."""
+        mocked_getattr.return_value = "redis_url"
+
+        # mock returns
+        mocked_connection.return_value = mock.MagicMock()
+        mocked_connection.return_value.__enter__.side_effect = True
+
+        # instantiates the class
+        redis_healthchecker = RedisHealthCheck()
+
+        # invokes the method check_status()
+        redis_healthchecker.check_status()
+        assert len(redis_healthchecker.errors), 0
+
+        # mock assertions
+        mocked_connection.assert_called_once_with("redis_url")
+
