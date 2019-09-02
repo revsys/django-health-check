394ababb72d2fc79d6473cb2b8753928266fa5d9
Ingvald Lorentzen <ingvald.lorentzen@intility.no>
2019-09-02 10:49:01 +0200

    - Fixed logging level - Added `redis_url` as class attribute for subclassing

--- health_check/contrib/redis/backends.py
+++ health_check/contrib/redis/backends.py
@@ -1,7 +1,7 @@
 import logging
 
 from django.conf import settings
-from redis import exceptions, from_url
+from redis import from_url, exceptions
 
 from health_check.backends import BaseHealthCheckBackend
 from health_check.exceptions import ServiceUnavailable
@@ -12,18 +12,18 @@ logger = logging.getLogger(__name__)
 class RedisHealthCheck(BaseHealthCheckBackend):
     """Health check for Redis."""
 
+    logger.debug("Checking for a redis_url in django settings...")
+    redis_url = getattr(settings, "REDIS_URL", None)
+
     def check_status(self):
-        """Check Redis service by opening and closing a connection."""
-        logger.debug("Checking for a redis_url in django settings...")
+        """Check Redis service by pinging the redis instance with a redis connection"""
 
-        redis_url = getattr(settings, "REDIS_URL", None)
-
-        logger.debug("Got %s as the redis_url. Connecting to redis...", redis_url)
+        logger.debug("Got %s as the redis_url. Connecting to redis...", self.redis_url)
 
         logger.debug("Attempting to connect to redis...")
         try:
             # conn is used as a context to release opened resources later
-            with from_url(redis_url) as conn:
+            with from_url(self.redis_url) as conn:
                 conn.ping()  # exceptions may be raised upon ping
         except ConnectionRefusedError as e:
             self.add_error(ServiceUnavailable("Unable to connect to Redis: Connection was refused."), e)
@@ -34,4 +34,4 @@ class RedisHealthCheck(BaseHealthCheckBackend):
         except BaseException as e:
             self.add_error(ServiceUnavailable("Unknown error"), e)
         else:
-            logger.info("Connection established. Redis is healthy.")
+            logger.debug("Connection established. Redis is healthy.")
