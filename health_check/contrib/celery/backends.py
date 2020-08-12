4c11082946094c2783a199d4a39882a0bd088e6c
Nick Werle <nick.werle@tenet3.com>
2020-08-12 13:02:25 -0400

    Added more detail for celery task timeouts - More detailed message for cases where a task takes too long to start - More detailed message for cases where a task takes too long to return a result - Added HEALTHCHECK_CELERY_QUEUE_TIMEOUT, which sets the maximum time for a task to wait in queue, defaults to HEALTHCHECK_CELERY_TIMEOUT (which defaults to 3) - Added HEALTHCHECK_CELERY_RESULT_TIMEOUT, which sets the maximum time for a task to return a result, defaults to HEALTHCHECK_CELERY_TIMEOUT (which defaults to 3)

--- health_check/contrib/celery/backends.py
+++ health_check/contrib/celery/backends.py
@@ -6,19 +6,22 @@ from health_check.exceptions import (
 )
 
 from .tasks import add
+from celery.exceptions import TaskRevokedError, TimeoutError
 
 
 class CeleryHealthCheck(BaseHealthCheckBackend):
     def check_status(self):
         timeout = getattr(settings, 'HEALTHCHECK_CELERY_TIMEOUT', 3)
+        result_timeout = getattr(settings, 'HEALTHCHECK_CELERY_RESULT_TIMEOUT', timeout)
+        queue_timeout = getattr(settings, 'HEALTHCHECK_CELERY_QUEUE_TIMEOUT', timeout)
 
         try:
             result = add.apply_async(
                 args=[4, 4],
-                expires=timeout,
+                expires=queue_timeout,
                 queue=self.queue
             )
-            result.get(timeout=timeout)
+            result.get(timeout=result_timeout)
             if result.result != 8:
                 self.add_error(ServiceReturnedUnexpectedResult("Celery returned wrong result"))
             add.forget()
@@ -26,5 +29,9 @@ class CeleryHealthCheck(BaseHealthCheckBackend):
             self.add_error(ServiceUnavailable("IOError"), e)
         except NotImplementedError as e:
             self.add_error(ServiceUnavailable("NotImplementedError: Make sure CELERY_RESULT_BACKEND is set"), e)
+        except TaskRevokedError as e:
+            self.add_error(ServiceUnavailable("TaskRevokedError: The task was revoked, likely because it spent too long in the queue"), e)
+        except TimeoutError as e:
+            self.add_error(ServiceUnavailable("TimeoutError: The task took too long to return a result"), e)
         except BaseException as e:
             self.add_error(ServiceUnavailable("Unknown error"), e)
