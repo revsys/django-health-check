93e9cdd1e881f255166df2d1189fcae838a077a9
tobias-fyi <tobyreaper@gmail.com>
2020-09-29 09:09:13 -0600

    Addresses issue #265 - use django.urls.path instead of django.conf.urls.url

--- health_check/urls.py
+++ health_check/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 
 from health_check.views import MainView
 
 app_name = 'health_check'
 
 urlpatterns = [
-    url(r'^$', MainView.as_view(), name='health_check_home'),
+    path('', MainView.as_view(), name='health_check_home'),
 ]
