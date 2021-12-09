e81eebd95df03b7af451f9f9ecd11b41a80bd774
Hasan Ramezani <hasan.r67@gmail.com>
2021-12-09 20:19:38 +0100

    Replace url with re_path.

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import include, url
+from django.urls import include, re_path
 
 urlpatterns = [
-    url(r'^ht/', include('health_check.urls')),
+    re_path(r'^ht/', include('health_check.urls')),
 ]
