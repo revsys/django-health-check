ad0b1e8b263a150dacc6f77896d8b10789028dcb
Pan Teparak <panteparak@me.com>
2023-11-29 12:41:04 +0700

    feat: add commmand tests

--- health_check/management/commands/health_check.py
+++ health_check/management/commands/health_check.py
@@ -1,6 +1,7 @@
 import sys
 
 from django.core.management.base import BaseCommand
+from django.http import Http404
 
 from health_check.mixins import CheckMixin
 
@@ -9,13 +10,17 @@ class Command(CheckMixin, BaseCommand):
     help = "Run health checks and exit 0 if everything went well."
 
     def add_arguments(self, parser):
-        parser.add_argument("--subset", type=str, default=None, nargs=1)
+        parser.add_argument("-s", '--subset', type=str, nargs=1)
 
     def handle(self, *args, **options):
         # perform all checks
         subset = options.get("subset", [])
         subset = subset[0] if subset else None
-        errors = self.check(subset=subset)
+        try:
+            errors = self.check(subset=subset)
+        except Http404 as e:
+            self.stdout.write(str(e))
+            sys.exit(1)
 
         for plugin_identifier, plugin in self.filter_plugins(subset=subset).items():
             style_func = self.style.SUCCESS if not plugin.errors else self.style.ERROR
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -4,6 +4,7 @@ import pytest
 from django.core.management import call_command
 
 from health_check.backends import BaseHealthCheckBackend
+from health_check.conf import HEALTH_CHECK
 from health_check.plugins import plugin_dir
 
 
@@ -34,4 +35,50 @@ class TestCommand:
         assert stdout.read() == (
             "FailPlugin               ... unknown error: Oops\n"
             "OkPlugin                 ... working\n"
+        )
+
+    def test_command_with_subset(self):
+        SUBSET_NAME_1 = 'subset-1'
+        SUBSET_NAME_2 = 'subset-2'
+        HEALTH_CHECK['SUBSETS'] = {
+            SUBSET_NAME_1: ["OkPlugin"],
+            SUBSET_NAME_2: ["OkPlugin", "FailPlugin"]
+        }
+
+        stdout = StringIO()
+        call_command(f"health_check", f"--subset={SUBSET_NAME_1}", stdout=stdout)
+        stdout.seek(0)
+        assert stdout.read() == (
+            "OkPlugin                 ... working\n"
+        )
+
+
+    def test_command_with_failed_check_subset(self):
+        SUBSET_NAME = 'subset-2'
+        HEALTH_CHECK['SUBSETS'] = {
+            SUBSET_NAME: ["OkPlugin", "FailPlugin"]
+        }
+
+        stdout = StringIO()
+        with pytest.raises(SystemExit):
+            call_command(f"health_check", f"--subset={SUBSET_NAME}", stdout=stdout)
+        stdout.seek(0)
+        assert stdout.read() == (
+            "FailPlugin               ... unknown error: Oops\n"
+            "OkPlugin                 ... working\n"
+        )
+
+    def test_command_with_non_existence_subset(self):
+        SUBSET_NAME = 'subset-2'
+        NON_EXISTENCE_SUBSET_NAME = "abcdef12"
+        HEALTH_CHECK['SUBSETS'] = {
+            SUBSET_NAME: ["OkPlugin"]
+        }
+
+        stdout = StringIO()
+        with pytest.raises(SystemExit):
+            call_command(f"health_check", f"--subset={NON_EXISTENCE_SUBSET_NAME}", stdout=stdout)
+        stdout.seek(0)
+        assert stdout.read() == (
+            f"Specify subset: '{NON_EXISTENCE_SUBSET_NAME}' does not exists.\n"
         )
