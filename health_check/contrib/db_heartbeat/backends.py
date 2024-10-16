2b1a92b311a1f99df1670b73d14c6763c6207ead
Pan Teparak <panteparak@me.com>
2024-10-16 16:47:05 +0700

    feat: add db_heartbeat

---
+++ health_check/contrib/db_heartbeat/backends.py
@@ -0,0 +1,21 @@
+from health_check.backends import BaseHealthCheckBackend
+from health_check.exceptions import ServiceUnavailable, ServiceReturnedUnexpectedResult
+from django.db import connection
+
+
+class DatabaseHeartBeatCheck(BaseHealthCheckBackend):
+    """
+    Health check that runs a simple SELECT 1; query to test if the database connection is alive.
+    """
+
+    def check_status(self):
+        try:
+            result = None
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT 1;")
+                result = cursor.fetchone()
+
+            if result != (1,):
+                raise ServiceReturnedUnexpectedResult("Health Check query did not return the expected result.")
+        except Exception as e:
+            raise ServiceUnavailable(f"Database health check failed: {e}")
