96dd3bc4aa6e60aa0c4f6d1fa416372c84fb2e42
Johannes Maron <johannes@maron.family>
2025-06-03 09:52:15 +0200

    Adapt PEP 612 (pyproject.toml) package metadata
    * Move all configuration to pyproject.toml file
    * Switch PyPi release process to OpenID Connect (OIDC)

---
+++ health_check/__init__.py
@@ -0,0 +1,6 @@
+"""Run a full check on the deployment,using a number of plugins to check internal and external services."""
+
+from . import _version  # noqa
+
+__version__ = _version.__version__
+VERSION = _version.VERSION_TUPLE
