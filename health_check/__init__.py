revsys/django-health-check

commit 96dd3bc4aa6e60aa0c4f6d1fa416372c84fb2e42

Adapt PEP 612 (pyproject.toml) package metadata
* Move all configuration to pyproject.toml file
* Switch PyPi release process to OpenID Connect (OIDC)

health_check/__init__.py
@@ -1,0 +1,6 @@
+"""Run a full check on the deployment,using a number of plugins to check internal and external services."""
+
+from . import _version  # noqa
+
+__version__ = _version.__version__
+VERSION = _version.VERSION_TUPLE

.editorconfig
@@ -27,7 +27,7 @@ not_skip = __init__.py
 indent_style = space
 indent_size = 4
 
-[*.{yml,html,xml,xsl,json}]
+[*.{yml,html,xml,xsl,json,toml}]
 indent_style = space
 indent_size = 2
 

.github/workflows/ci.yml
@@ -21,10 +21,10 @@ jobs:
       - uses: actions/checkout@v3
       - uses: actions/setup-python@v5
         with:
-          python-version: "3.10"
+          python-version: "3.x"
           cache: 'pip'
-          cache-dependency-path: 'linter-requirements.txt'
-      - run: python -m pip install -r linter-requirements.txt
+          cache-dependency-path: '**/pyproject.toml'
+      - run: python -m pip install -e .[lint]
       - run: ${{ matrix.lint-command }}
 
   dist:
@@ -37,7 +37,7 @@ jobs:
           python-version: "3.x"
           cache: 'pip'
           cache-dependency-path: '**/pyproject.toml'
-      - run: python -m pip install --upgrade pip build wheel twine
+      - run: python -m pip install --upgrade pip build twine
       - run: python -m build --sdist --wheel
       - run: python -m twine check dist/*
 

.github/workflows/release.yml
@@ -3,19 +3,38 @@ name: Release
 on:
   release:
     types: [published]
+  workflow_dispatch:
+
+permissions:
+  id-token: write
 
 jobs:
 
-  PyPi:
+  release-build:
     runs-on: ubuntu-latest
+
     steps:
-    - uses: actions/checkout@v3
-    - uses: actions/setup-python@v5
-      with:
-        python-version: "3.x"
-    - run: python -m pip install --upgrade pip build wheel twine
-    - run: python -m build --sdist --wheel
-    - run: python -m twine upload dist/*
-      env:
-        TWINE_USERNAME: __token__
-        TWINE_PASSWORD: ${{ secrets.TWINE_PASSWORD }}
+      - uses: actions/checkout@v4
+      - uses: actions/setup-python@v5
+        with:
+          python-version: "3.x"
+      - run: python -m pip install --upgrade pip build
+      - run: python -m build --sdist --wheel
+      - uses: actions/upload-artifact@v4
+        with:
+          name: release-dists
+          path: dist/
+
+  pypi-publish:
+    runs-on: ubuntu-latest
+    needs:
+      - release-build
+    permissions:
+      id-token: write
+
+    steps:
+      - uses: actions/download-artifact@v4
+        with:
+          name: release-dists
+          path: dist/
+      - uses: pypa/gh-action-pypi-publish@release/v1

.gitignore
@@ -109,3 +109,6 @@ ENV/
 # mac
 .DS_Store
 .aider*
+
+# SCM
+_version.py

MANIFEST.in
@@ -1,3 +1,0 @@
-include LICENSE
-prune tests
-prune docs

pyproject.toml
@@ -1,5 +1,71 @@
-[tool.black]
-target-version = ["py38"]
+[build-system]
+requires = ["flit_core>=3.2", "flit_scm", "wheel"]
+build-backend = "flit_scm:buildapi"
+
+[project]
+name = "django-health-check"
+readme = { file = "README.md", content-type = "text/markdown" }
+license = { file = "LICENSE" }
+authors = [
+  { name = "Kristian Ollegaard", email = "kristian@oellegaard.com" },
+  { name = "Johannes Maron", email = "johannes@maron.family" }
+]
+homepage = "https://github.com/revsys/django-health-check"
+keywords = ["django", "postgresql"]
+classifiers = [
+  "Development Status :: 5 - Production/Stable",
+  "Framework :: Django",
+  "Framework :: Django :: 4.2",
+  "Framework :: Django :: 5.2",
+  "Intended Audience :: Developers",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  "Programming Language :: Python",
+  "Programming Language :: Python :: 3",
+  "Programming Language :: Python :: 3 :: Only",
+  "Programming Language :: Python :: 3.9",
+  "Programming Language :: Python :: 3.10",
+  "Programming Language :: Python :: 3.11",
+  "Programming Language :: Python :: 3.12",
+  "Programming Language :: Python :: 3.13",
+  "Topic :: Software Development :: Quality Assurance",
+  "Topic :: System :: Logging",
+  "Topic :: System :: Monitoring",
+  "Topic :: Utilities"
+]
+requires-python = ">=3.9"
+dynamic = [
+  "version",
+  "description",
+]
+dependencies = [
+  "Django>=4.2",
+]
+
+[project.optional-dependencies]
+test = [
+  "pytest",
+  "pytest-cov",
+  "pytest-django",
+  "celery",
+  "redis",
+  "django-storages",
+  "boto3",
+]
+docs = ["sphinx"]
+lint = [
+  "bandit==1.8.3",
+  "black==23.12.1",
+  "isort==6.0.1",
+  "flake8==7.0.0",
+  "pydocstyle[toml]==6.2.3",
+]
+
+[tool.flit.module]
+name = "health_check"
+
+[tool.setuptools_scm]
+write_to = "health_check/_version.py"
 
 [tool.pytest.ini_options]
 addopts = "--nomigrations --reuse-db --strict-markers"
@@ -7,3 +73,37 @@ DJANGO_SETTINGS_MODULE = "tests.testapp.settings"
 norecursedirs = ".git"
 python_files = "test_*.py"
 xfail_strict = true
+
+[tool.coverage.run]
+branch = true
+omit = [
+  "*/migrations/*",
+  "*/tests/*",
+  "*/test_*.py",
+  ".eggs/*"
+]
+
+[tool.coverage.report]
+ignore_errors = true
+show_missing = true
+skip_covered = true
+sort = "Cover"
+
+[tool.isort]
+atomic = true
+line_length = 88
+multi_line_output = 3
+include_trailing_comma = true
+force_grid_wrap = 0
+use_parentheses = true
+known_first_party = ["health_check", "tests"]
+default_section = "THIRDPARTY"
+combine_as_imports = true
+skip = ["health_check/_version.py"]
+
+[tool.pydocstyle]
+add-ignore = ["D1"]
+match-dir = "(?!tests|env|.eggs|\\.).*"
+
+[tool.black]
+target-version = ["py39"]

setup.cfg
@@ -1,107 +1,4 @@
-[metadata]
-name = django-health-check
-author = Kristian Ollegaard
-author_email = kristian@oellegaard.com
-description = Run checks on services like databases, queue servers, celery processes, etc.
-long_description = file: README.md
-long_description_content_type = text/markdown
-url = https://github.com/revsys/django-health-check
-license = MIT License
-license_file = LICENSE
-classifier =
-  Development Status :: 5 - Production/Stable
-  Framework :: Django
-  Framework :: Django :: 4.2
-  Framework :: Django :: 5.2
-  Intended Audience :: Developers
-  License :: OSI Approved :: MIT License
-  Operating System :: OS Independent
-  Programming Language :: Python
-  Programming Language :: Python :: 3
-  Programming Language :: Python :: 3 :: Only
-  Programming Language :: Python :: 3.9
-  Programming Language :: Python :: 3.10
-  Programming Language :: Python :: 3.11
-  Programming Language :: Python :: 3.12
-  Programming Language :: Python :: 3.13
-  Topic :: Software Development :: Quality Assurance
-  Topic :: System :: Logging
-  Topic :: System :: Monitoring
-  Topic :: Utilities
-keywords =
-  django
-  postgresql
-
-[options]
-python_requires = >=3.9
-include_package_data = True
-packages = health_check
-install_requires =
-    django>=2.2
-setup_requires =
-    setuptools_scm
-
-[options.extras_require]
-test =
-    pytest
-    pytest-cov
-    pytest-django
-    celery
-    redis
-    django-storages
-    boto3
-docs =
-    sphinx
-
-[bdist_wheel]
-universal = 1
-
-[bdist_rpm]
-requires =
-    python-django-appconf >= 2.0
-    python-django-appconf >= 0.6
-
-[aliases]
-test = pytest
-
-[tool:pytest]
-norecursedirs=venv env .eggs
-DJANGO_SETTINGS_MODULE=tests.testapp.settings
-addopts =
-    --tb=short
-    -rxs
-    --cov=health_check
-
 [flake8]
 max-line-length = 88
 select = C,E,F,W,B,B950
 ignore = E203, E501, W503, E731
-
-[isort]
-atomic = true
-line_length = 88
-multi_line_output = 3
-include_trailing_comma = True
-force_grid_wrap = 0
-use_parentheses = True
-known_first_party = health_check, tests
-default_section = THIRDPARTY
-combine_as_imports = true
-
-[pydocstyle]
-add-ignore = D1
-match-dir = (?!tests|env|.eggs|\.).*
-
-[coverage:run]
-branch = True
-omit =
-  */migrations/*
-  */tests/*
-  */test_*.py
-  .eggs/*
-
-[coverage:report]
-ignore_errors = True
-show_missing = True
-skip_covered = True
-sort = Cover

commit c56e48e014b5d59d84f9ba2e3286f4da36ac7ca7

Repair VERSION variable
VERSION_TUPLE only resolves to an uninitialized object or tuple
(depending on whether type checking is enabled). The real version tuple
is contained in __version_tuple__.

Fixes: 96dd3bc4aa6e ("Adapt PEP 612 (pyproject.toml) package metadata")
Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

health_check/__init__.py
@@ -3,4 +3,4 @@
 from . import _version  # noqa
 
 __version__ = _version.__version__
-VERSION = _version.VERSION_TUPLE
+VERSION = _version.__version_tuple__